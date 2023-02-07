618d609aec36c6347e434c29a4fc1c63e2f57ded
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-07 22:57:07 +0200

    complete video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+
+const savedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+if (savedTime) {
+  player.setCurrentTime(savedTime.seconds);
+}
+
+player.on('timeupdate', throttle(onPlayerStart, 1000));
+
+function onPlayerStart(e) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify({
+      seconds: e.seconds,
+    })
+  );
+}
