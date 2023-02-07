062da6fdc530b0c59f4fde110214f6b8c45f7aaf
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-08 00:11:13 +0200

    update video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,12 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframeEl = document.querySelector('iframe');
 const player = new Player(iframeEl);
 
-const savedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+const savedTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
 if (savedTime) {
   player.setCurrentTime(savedTime.seconds);
@@ -14,7 +16,7 @@ player.on('timeupdate', throttle(onPlayerStart, 1000));
 
 function onPlayerStart(e) {
   localStorage.setItem(
-    'videoplayer-current-time',
+    STORAGE_KEY,
     JSON.stringify({
       seconds: e.seconds,
     })
