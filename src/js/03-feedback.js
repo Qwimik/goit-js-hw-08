3d8a7a88921266cb2ebd66b3d8a4ccfa1e29f7bc
Oleksii Rozhnenko <qwimik@gmail.com>
2023-03-01 17:45:49 +0200

    add trim

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,5 @@
 import throttle from 'lodash.throttle';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const STORAGE_KEY = 'feedback-form-state';
 const formData = JSON.parse(localStorage.getItem(STORAGE_KEY))
@@ -13,6 +14,13 @@ savedInputs();
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  if (input.value == '' && textarea.value == '') {
+    return Notify.failure('Please fill out all fields!');
+  }
+  if (input.value.trim() == '' || textarea.value.trim() == '') {
+    return Notify.warning('Please fill out ALL fields!!!');
+  }
+  Notify.success('Thank you! All is well :)');
   console.log(formData);
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
@@ -21,17 +29,15 @@ form.addEventListener('submit', e => {
 form.addEventListener('input', throttle(onInputChange, 500));
 
 function onInputChange(e) {
-  formData[e.target.name] = e.target.value;
+  formData[e.target.name] = e.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function savedInputs() {
-  if (formData) {
-    if (formData.email) {
-      input.value = formData.email;
-    }
-    if (formData.message) {
-      textarea.value = formData.message;
-    }
+  if (formData.email) {
+    input.value = formData.email;
+  }
+  if (formData.message) {
+    textarea.value = formData.message;
   }
 }
